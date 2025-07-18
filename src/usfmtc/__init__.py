8946721148fd5ca91f78ac6c328bfcda5d4bb37a
Martin Hosken <martin_hosken@sil.org>
2025-07-18 11:40:49 +0700

    Fix addintro for getrefs

--- src/usfmtc/__init__.py
+++ src/usfmtc/__init__.py
@@ -223,7 +223,8 @@ class USX:
         root = self.getroot()
         res = root.__class__(root.tag, attrib=root.attrib)
         for (start, end, r) in self._procrefs(*refs, skiptest=skiptest):
-            subdoc = start.copy_range(root, end, addintro=(addintro and not i), headers=headers, chapters=chapters, grammar=self.grammar)
+            subdoc = start.copy_range(root, end, addintro=(addintro and r.first.chapter == 1),
+                                      headers=headers, chapters=chapters, grammar=self.grammar)
             if len(subdoc):
                 subdoc[0].set("vid", str(r.first))
                 for e in subdoc:
