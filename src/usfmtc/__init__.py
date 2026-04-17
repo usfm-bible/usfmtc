f8285cc8773a9530829eaa5ecde6b414a486137d
Martin Hosken <martin_hosken@sil.org>
2026-04-17 09:10:59 +0700

    Releasing 0.4.7

--- src/usfmtc/__init__.py
+++ src/usfmtc/__init__.py
@@ -26,7 +26,7 @@ from usfmtc.usfmgenerate import usx2usfm
 from usfmtc.reference import RefList
 import xml.etree.ElementTree as et
 
-version = "0.4.6"
+version = "0.4.7"
 
 def _grammarDoc(gsrc, extensions=[], factory=et):
     data = readsrc(gsrc)
@@ -303,7 +303,7 @@ class USX:
             if outtype == "usfm3.0":
                 outtype = "usfm"
                 if version is None:
-                    version = "0.4.6"
+                    version = "0.4.7"
             self.outUsfm(grammar=grammar, file=outfpath, outversion=version, altparser=altparser, **kw)
 
     def canonicalise(self, version=None):
@@ -349,7 +349,7 @@ class USX:
     @version.setter
     def version(self, version):
         if isinstance(version, (list, tuple)):
-            version = "0.4.6".join([str(x) for x in version])
+            version = "0.4.7".join([str(x) for x in version])
         if version is not None:
             self.getroot().set('version', str(version))
 
